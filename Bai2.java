e49f5afd1c2758791ec6e5d564fed8f9c1494639
Bui The Thuat <92557177+thuat03@users.noreply.github.com>
2022-06-17 08:59:32 +0700

    Variables

---
+++ Bai2.java
@@ -0,0 +1,17 @@
+public class Bai2 {
+    public static void main(String[] args)
+    {
+        int x=6;
+        float y=3.14f;
+        double z=2.818;
+        boolean t=false;
+        String name="thuat";
+
+        System.out.println(x);
+        System.out.println("float: "+ y);
+        System.out.println(x+z);
+        System.out.println("name: "+name);
+        System.out.println(t);
+    }
+
+}
