12001041b5411312f721997befa709d9a538af9b
Bui The Thuat <92557177+thuat03@users.noreply.github.com>
2022-06-23 23:42:13 +0700

    Printf (similar to printf in C language)

---
+++ Bai13.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Bai13 {
+    
+
+
+    public static void main(String[]args)
+    {
+        int a=2;
+        float b=3.14f;
+        boolean bool=true;
+        char c='$';
+        String name ="Thuat";
+        double e=2.818;
+        System.out.printf("This is a int value: %d\n",a);
+        System.out.printf("This is '%b' a boolean value:\n",bool);
+        System.out.printf("This is a float value: %.4f\n",b);
+        System.out.printf("This is a string: %s\n",name);
+        System.out.printf("This is a double value: %.5f %.4f %.3f %c",e,e,e,c);
+    }
+}
