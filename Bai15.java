a083567693426c3635d18dcca6309885cc40354d
Bui The Thuat <92557177+thuat03@users.noreply.github.com>
2022-06-26 00:04:43 +0700

    Exceptions

---
+++ Bai15.java
@@ -0,0 +1,18 @@
+public class Bai15 {
+    public static void main(String[]args)
+    {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[3]);
+            System.out.println(myNumbers[1]);
+
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } 
+        finally
+        {
+            System.out.println("The 'try catch' is finished.");
+        }
+    }
+}
